a47260e658f39d2a8511441d85879e62114d8fc2
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-05-21 10:40:10 +0530

    Add: Code for left and right arms

--- Robotic_Arm_Arduino_Code/src/main.cpp
+++ Robotic_Arm_Arduino_Code/src/main.cpp
@@ -2,39 +2,78 @@
 #include <Servo.h>
 #include <ArduinoJson.h>
 
-#define SERVO_PIN_BASE_0 3  // Base 0-180 is possible 
-#define SERVO_PIN_ARM1_1 5  // Arm1 Do not go below 50 degree (50-180)
-#define SERVO_PIN_ARM2_2 6  // Arm2 0-180 is possible 
-#define SERVO_PIN_WRIST_3 9   // Wrist 0-180
-#define SERVO_PIN_LEFT_CLAW_4 10 // Left Claw 0-180
-#define SERVO_PIN_RIGHT_CLAW_5 11  // Right Claw 0-180
+// Right Arm
+#define L_SERVO_PIN_BASE_0 2  // Base 0-180 is possible 
+#define L_SERVO_PIN_ARM1_1 3  // Base 0-180 is possible 
+#define L_SERVO_PIN_ARM2_2 4
+#define L_SERVO_PIN_WRIST_3 5   // Wrist 0-180
+#define L_SERVO_PIN_LEFT_CLAW_4 6 // Left Claw 0-180
+#define L_SERVO_PIN_RIGHT_CLAW_5 7  // Right Claw 0-180
 
-Servo SERVO_MOTOR_BASE_0;
-Servo SERVO_MOTOR_ARM1_1;
-Servo SERVO_MOTOR_ARM2_2;
-Servo SERVO_MOTOR_WRIST_3;
-Servo SERVO_MOTOR_LEFT_CLAW_4;
-Servo SERVO_MOTOR_RIGHT_CLAW_5;
+Servo L_SERVO_MOTOR_BASE_0;
+Servo L_SERVO_MOTOR_ARM1_1;
+Servo L_SERVO_MOTOR_ARM2_2;
+Servo L_SERVO_MOTOR_WRIST_3;
+Servo L_SERVO_MOTOR_LEFT_CLAW_4;
+Servo L_SERVO_MOTOR_RIGHT_CLAW_5;
 
-void base(int angle);
-void arm1(int angle);
-void arm2(int angle);
-void wrist(int angle);
-void left_claw(int angle);
-void right_claw(int angle);
+// Left Arm
+#define R_SERVO_PIN_BASE_0 8  // Base 0-180 is possible 
+#define R_SERVO_PIN_ARM1_1 9  // Base 0-180 is possible 
+#define R_SERVO_PIN_ARM2_2 10
+#define R_SERVO_PIN_WRIST_3 11   // Wrist 0-180
+#define R_SERVO_PIN_LEFT_CLAW_4 12 // Left Claw 0-180
+#define R_SERVO_PIN_RIGHT_CLAW_5 13  // Right Claw 0-180
 
-void set_default_position();
-void pick_object();
-void drop_object();
+Servo R_SERVO_MOTOR_BASE_0;
+Servo R_SERVO_MOTOR_ARM1_1;
+Servo R_SERVO_MOTOR_ARM2_2;
+Servo R_SERVO_MOTOR_WRIST_3;
+Servo R_SERVO_MOTOR_LEFT_CLAW_4;
+Servo R_SERVO_MOTOR_RIGHT_CLAW_5;
+
+
+void left_arm_base(int angle);
+void left_arm_arm1(int angle);
+void left_arm_arm2(int angle);
+void left_arm_wrist(int angle);
+void left_arm_left_claw(int angle);
+void left_arm_right_claw(int angle);
+
+void left_arm_set_default_position();
+void left_arm_pick_object();
+void left_arm_drop_object();
+
+// ------------------------------------- Right Arm
+
+void right_arm_base(int angle);
+void right_arm_arm1(int angle);
+void right_arm_arm2(int angle);
+void right_arm_wrist(int angle);
+void right_arm_left_claw(int angle);
+void right_arm_right_claw(int angle);
+
+void right_arm_set_default_position();
+void right_arm_pick_object();
+void right_arm_drop_object();
+
 
 void setup(){
   Serial.begin(9600);
-  SERVO_MOTOR_BASE_0.attach(SERVO_PIN_BASE_0); // Base
-  SERVO_MOTOR_ARM1_1.attach(SERVO_PIN_ARM1_1); // Arm 1
-  SERVO_MOTOR_ARM2_2.attach(SERVO_PIN_ARM2_2); // Arm 2
-  SERVO_MOTOR_WRIST_3.attach(SERVO_PIN_WRIST_3); // Wrist
-  SERVO_MOTOR_LEFT_CLAW_4.attach(SERVO_PIN_LEFT_CLAW_4); // Left
-  SERVO_MOTOR_RIGHT_CLAW_5.attach(SERVO_PIN_RIGHT_CLAW_5); // Right
+
+  L_SERVO_MOTOR_BASE_0.attach(L_SERVO_PIN_BASE_0); // Base
+  L_SERVO_MOTOR_ARM1_1.attach(L_SERVO_PIN_ARM1_1); // Arm 1
+  L_SERVO_MOTOR_ARM2_2.attach(L_SERVO_PIN_ARM2_2); // Arm 2
+  L_SERVO_MOTOR_WRIST_3.attach(L_SERVO_PIN_WRIST_3); // Wrist
+  L_SERVO_MOTOR_LEFT_CLAW_4.attach(L_SERVO_PIN_LEFT_CLAW_4); // Left
+  L_SERVO_MOTOR_RIGHT_CLAW_5.attach(L_SERVO_PIN_RIGHT_CLAW_5); // Right
+
+  R_SERVO_MOTOR_BASE_0.attach(R_SERVO_PIN_BASE_0); // Base
+  R_SERVO_MOTOR_ARM1_1.attach(R_SERVO_PIN_ARM1_1); // Arm 1
+  R_SERVO_MOTOR_ARM2_2.attach(R_SERVO_PIN_ARM2_2); // Arm 2
+  R_SERVO_MOTOR_WRIST_3.attach(R_SERVO_PIN_WRIST_3); // Wrist
+  R_SERVO_MOTOR_LEFT_CLAW_4.attach(R_SERVO_PIN_LEFT_CLAW_4); // Left
+  R_SERVO_MOTOR_RIGHT_CLAW_5.attach(R_SERVO_PIN_RIGHT_CLAW_5); // Right
 }
 
 void loop(){
@@ -54,32 +93,63 @@ void loop(){
     String motor = doc["motor"];
     String angle = doc["angle"];
     
-    if (motor == "base")
-    base(angle.toInt());
+    // ------------ Left Arm Code --------------
+
+    if (motor == "l_base")
+      left_arm_base(angle.toInt());
     
-    else if (motor == "arm1")
-    arm1(angle.toInt());
+    else if (motor == "l_arm1")
+      left_arm_arm1(angle.toInt());
     
-    else if (motor == "arm2")
-    arm2(angle.toInt());
+    else if (motor == "l_arm2")
+      left_arm_arm2(angle.toInt());
     
-    else if (motor == "wrist")
-      wrist(angle.toInt());
+    else if (motor == "l_wrist")
+      left_arm_wrist(angle.toInt());
     
-    else if (motor == "left_claw")
-      left_claw(angle.toInt());
+    else if (motor == "l_left_claw")
+      left_arm_left_claw(angle.toInt());
     
-    else if (motor == "right_claw")
-      right_claw(angle.toInt());
+    else if (motor == "l_right_claw")
+      left_arm_right_claw(angle.toInt());
     
-    else if (motor == "set_default_position")
-      set_default_position();
+    else if (motor == "l_set_default_position")
+      left_arm_set_default_position();
     
-    else if (motor == "pick_object")
-      pick_object();
+    else if (motor == "l_pick_object")
+      left_arm_pick_object();
     
-    else if (motor == "drop_object")
-      drop_object();
+    else if (motor == "l_drop_object")
+      left_arm_drop_object();
+
+    // --------- Right Arm Code ---------------
+
+    if (motor == "r_base")
+    right_arm_base(angle.toInt());
+    
+    else if (motor == "r_arm1")
+    right_arm_arm1(angle.toInt());
+    
+    else if (motor == "r_arm2")
+    right_arm_arm2(angle.toInt());
+    
+    else if (motor == "r_wrist")
+      right_arm_wrist(angle.toInt());
+    
+    else if (motor == "r_left_claw")
+      right_arm_left_claw(angle.toInt());
+    
+    else if (motor == "r_right_claw")
+      right_arm_right_claw(angle.toInt());
+    
+    else if (motor == "r_set_default_position")
+      right_arm_set_default_position();
+    
+    else if (motor == "r_pick_object")
+      right_arm_pick_object();
+    
+    else if (motor == "r_drop_object")
+      right_arm_drop_object();
 
     else{
         Serial.println("Invalid command received");
@@ -89,147 +159,296 @@ void loop(){
 
 }
 
-void base(int angle){
-  SERVO_MOTOR_BASE_0.write(angle);
+void left_arm_base(int angle){
+  L_SERVO_MOTOR_BASE_0.write(angle);
   Serial.print("Base at: ");
   Serial.println(angle);
 }
 
-void arm1(int angle){
+void left_arm_arm1(int angle){
   // 180 - Straight up
-  SERVO_MOTOR_ARM1_1.write(angle);
+  L_SERVO_MOTOR_ARM1_1.write(angle);
   Serial.print("Arm1 at: ");
   Serial.println(angle);
 }
 
-void arm2(int angle){
-  SERVO_MOTOR_ARM2_2.write(angle);
+void left_arm_arm2(int angle){
+  L_SERVO_MOTOR_ARM2_2.write(angle);
   Serial.print("Arm2 at: ");
   Serial.println(angle);
 }
 
-void wrist(int angle){
-  SERVO_MOTOR_WRIST_3.write(angle);
+void left_arm_wrist(int angle){
+  L_SERVO_MOTOR_WRIST_3.write(angle);
   Serial.print("Wrist at: ");
   Serial.println(angle);
 }
 
-void left_claw(int angle){
-  SERVO_MOTOR_LEFT_CLAW_4.write(angle);
+void left_arm_left_claw(int angle){
+  L_SERVO_MOTOR_LEFT_CLAW_4.write(angle);
   Serial.print("Left Claw at: ");
   Serial.println(angle);
 }
 
-void right_claw(int angle){
-  SERVO_MOTOR_RIGHT_CLAW_5.write(angle);
+void left_arm_right_claw(int angle){
+  L_SERVO_MOTOR_RIGHT_CLAW_5.write(angle);
   Serial.print("Right Claw at: ");
   Serial.println(angle);
 }
 
-void set_default_position(){
+void left_arm_set_default_position(){
   // servo.read() -> returns the last position value written to the servo
   
   // get base current position
-  int base_angle = SERVO_MOTOR_BASE_0.read();
+  int left_arm_base_angle = L_SERVO_MOTOR_BASE_0.read();
 
   // get arm1 current position
-  int arm1_angle = SERVO_MOTOR_ARM1_1.read();  
+  int left_arm_arm1_angle = L_SERVO_MOTOR_ARM1_1.read();  
 
   // get arm2 current position
-  int arm2_angle = SERVO_MOTOR_ARM2_2.read();
+  int left_arm_arm2_angle = L_SERVO_MOTOR_ARM2_2.read();
   
   // get wrist current position
-  int wrist_angle = SERVO_MOTOR_WRIST_3.read();
+  int left_arm_wrist_angle = L_SERVO_MOTOR_WRIST_3.read();
 
   // get claw left current position
-  int left_claw_angle = SERVO_MOTOR_LEFT_CLAW_4.read();
+  int left_arm_left_claw_angle = L_SERVO_MOTOR_LEFT_CLAW_4.read();
 
   // get claw right current position
-  int right_claw_angle = SERVO_MOTOR_RIGHT_CLAW_5.read();
+  int left_arm_right_claw_angle = L_SERVO_MOTOR_RIGHT_CLAW_5.read();
   
   // print current arm positions
   Serial.print("Currnt Base angle: ");
-  Serial.println(base_angle);
+  Serial.println(left_arm_base_angle);
   
   Serial.print("Currnt arm1 angle: ");
-  Serial.println(arm1_angle);
+  Serial.println(left_arm_arm1_angle);
   
   Serial.print("Currnt arm2 angle: ");
-  Serial.println(arm2_angle);     
+  Serial.println(left_arm_arm2_angle);     
   
   Serial.print("Currnt wrist angle: ");
-  Serial.println(wrist_angle); 
+  Serial.println(left_arm_wrist_angle); 
   
   Serial.print("Currnt left angle: ");
-  Serial.println(left_claw_angle); 
+  Serial.println(left_arm_left_claw_angle); 
   
   Serial.print("Currnt right angle: ");
-  Serial.println(right_claw_angle);
+  Serial.println(left_arm_right_claw_angle);
 
   // move claw left to 90
-      if(left_claw_angle!=90)
-       SERVO_MOTOR_LEFT_CLAW_4.write(90);
- delay(200);
+      if(left_arm_left_claw_angle!=90)
+       L_SERVO_MOTOR_LEFT_CLAW_4.write(90);
+      delay(200);
 
   // move claw right to 90
-    if(right_claw_angle!=90)
-      SERVO_MOTOR_RIGHT_CLAW_5.write(90);
+    if(left_arm_right_claw_angle!=90)
+      L_SERVO_MOTOR_RIGHT_CLAW_5.write(90);
  delay(200);
 
   // move arm1 to 90
-    if(arm1_angle!=90)
-        SERVO_MOTOR_ARM1_1.write(90);
+    if(left_arm_arm1_angle!=90)
+        L_SERVO_MOTOR_ARM1_1.write(90);
   delay(200);
    
   // move arm2 to 90
-    if(arm2_angle!=90)
-      SERVO_MOTOR_ARM2_2.write(90);
+    if(left_arm_arm2_angle!=90)
+      L_SERVO_MOTOR_ARM2_2.write(90);
    delay(200);
   
       // move base to 0
-    if(base_angle!=0)
-       SERVO_MOTOR_BASE_0.write(0);    
+    if(left_arm_base_angle!=0)
+       L_SERVO_MOTOR_BASE_0.write(0);    
     delay(200);
  
   // move wrist to 180
-      if(wrist_angle!=180)
-       SERVO_MOTOR_WRIST_3.write(180);
+      if(left_arm_wrist_angle!=180)
+       L_SERVO_MOTOR_WRIST_3.write(180);
      delay(200);
 
 }
 
-void pick_object(){
+void left_arm_pick_object(){
 
-  SERVO_MOTOR_BASE_0.write(0); // Base
+  L_SERVO_MOTOR_BASE_0.write(0); // Base
   delay(200);
- SERVO_MOTOR_ARM1_1.write(40); // Arm 1
-   delay(200);
-SERVO_MOTOR_ARM2_2.write(165); // Arm 2
-   delay(200);
-SERVO_MOTOR_WRIST_3.write(180); // Wrist
-   delay(200);
-SERVO_MOTOR_LEFT_CLAW_4.write(115); // Left claw
-   delay(200);
-SERVO_MOTOR_RIGHT_CLAW_5.write(70); // Right claw
- delay(200);
+  L_SERVO_MOTOR_ARM1_1.write(40); // Arm 1
+  delay(200);
+  L_SERVO_MOTOR_ARM2_2.write(165); // Arm 2
+  delay(200);
+  L_SERVO_MOTOR_WRIST_3.write(180); // Wrist
+  delay(200);
+  L_SERVO_MOTOR_LEFT_CLAW_4.write(115); // Left claw
+  delay(200);
+  L_SERVO_MOTOR_RIGHT_CLAW_5.write(70); // Right claw
+  delay(200);
 
   Serial.println("pick_object called");
 }
 
-void drop_object(){
+void left_arm_drop_object(){
   
-  SERVO_MOTOR_ARM2_2.write(150); // Arm 2
+  L_SERVO_MOTOR_ARM2_2.write(150); // Arm 2
   delay(200);
-  SERVO_MOTOR_ARM1_1.write(40); // Arm 1
-   delay(200);
-SERVO_MOTOR_BASE_0.write(90); // Base
-   delay(200);
-SERVO_MOTOR_WRIST_3.write(180); // Wrist
-   delay(200);
-SERVO_MOTOR_LEFT_CLAW_4.write(90); // Left claw
-   delay(200);
-SERVO_MOTOR_RIGHT_CLAW_5.write(90); // Right claw
+  L_SERVO_MOTOR_ARM1_1.write(40); // Arm 1
+  delay(200);
+  L_SERVO_MOTOR_BASE_0.write(90); // Base
+  delay(200);
+  L_SERVO_MOTOR_WRIST_3.write(180); // Wrist
+  delay(200);
+  L_SERVO_MOTOR_LEFT_CLAW_4.write(90); // Left claw
+  delay(200);
+  L_SERVO_MOTOR_RIGHT_CLAW_5.write(90); // Right claw
+  delay(200);
+
+  Serial.println("drop_object called");
+}
+
+
+// ------------------------------ Right Arm
+
+
+void right_arm_base(int angle){
+  R_SERVO_MOTOR_BASE_0.write(angle);
+  Serial.print("Base at: ");
+  Serial.println(angle);
+}
+
+void right_arm_arm1(int angle){
+  // 180 - Straight up
+  R_SERVO_MOTOR_ARM1_1.write(angle);
+  Serial.print("Arm1 at: ");
+  Serial.println(angle);
+}
+
+void right_arm_arm2(int angle){
+  R_SERVO_MOTOR_ARM2_2.write(angle);
+  Serial.print("Arm2 at: ");
+  Serial.println(angle);
+}
+
+void right_arm_wrist(int angle){
+  R_SERVO_MOTOR_WRIST_3.write(angle);
+  Serial.print("Wrist at: ");
+  Serial.println(angle);
+}
+
+void right_arm_left_claw(int angle){
+  R_SERVO_MOTOR_LEFT_CLAW_4.write(angle);
+  Serial.print("Left Claw at: ");
+  Serial.println(angle);
+}
+
+void right_arm_right_claw(int angle){
+  R_SERVO_MOTOR_RIGHT_CLAW_5.write(angle);
+  Serial.print("Right Claw at: ");
+  Serial.println(angle);
+}
+
+void right_arm_set_default_position(){
+  // servo.read() -> returns the last position value written to the servo
+  
+  // get base current position
+  int right_arm_base_angle = R_SERVO_MOTOR_BASE_0.read();
+
+  // get arm1 current position
+  int right_arm_arm1_angle = R_SERVO_MOTOR_ARM1_1.read();  
+
+  // get arm2 current position
+  int right_arm_arm2_angle = R_SERVO_MOTOR_ARM2_2.read();
+  
+  // get wrist current position
+  int right_arm_wrist_angle = R_SERVO_MOTOR_WRIST_3.read();
+
+  // get claw left current position
+  int right_arm_left_claw_angle = R_SERVO_MOTOR_LEFT_CLAW_4.read();
+
+  // get claw right current position
+  int right_arm_right_claw_angle = R_SERVO_MOTOR_RIGHT_CLAW_5.read();
+  
+  // print current arm positions
+  Serial.print("Currnt Base angle: ");
+  Serial.println(right_arm_base_angle);
+  
+  Serial.print("Currnt arm1 angle: ");
+  Serial.println(right_arm_arm1_angle);
+  
+  Serial.print("Currnt arm2 angle: ");
+  Serial.println(right_arm_arm2_angle);     
+  
+  Serial.print("Currnt wrist angle: ");
+  Serial.println(right_arm_wrist_angle); 
+  
+  Serial.print("Currnt left angle: ");
+  Serial.println(right_arm_left_claw_angle); 
+  
+  Serial.print("Currnt right angle: ");
+  Serial.println(right_arm_right_claw_angle);
+
+  // move claw left to 90
+      if(right_arm_left_claw_angle!=90)
+       R_SERVO_MOTOR_LEFT_CLAW_4.write(90);
+      delay(200);
+
+  // move claw right to 90
+    if(right_arm_right_claw_angle!=90)
+      R_SERVO_MOTOR_RIGHT_CLAW_5.write(90);
  delay(200);
+
+  // move arm1 to 90
+    if(right_arm_arm1_angle!=90)
+        R_SERVO_MOTOR_ARM1_1.write(90);
+  delay(200);
+   
+  // move arm2 to 90
+    if(right_arm_arm2_angle!=90)
+      R_SERVO_MOTOR_ARM2_2.write(90);
+   delay(200);
+  
+      // move base to 0
+    if(right_arm_base_angle!=0)
+       R_SERVO_MOTOR_BASE_0.write(0);    
+    delay(200);
+ 
+  // move wrist to 180
+      if(right_arm_wrist_angle!=180)
+       R_SERVO_MOTOR_WRIST_3.write(180);
+     delay(200);
+
+}
+
+void right_arm_pick_object(){
+
+  R_SERVO_MOTOR_BASE_0.write(0); // Base
+  delay(200);
+  R_SERVO_MOTOR_ARM1_1.write(40); // Arm 1
+  delay(200);
+  R_SERVO_MOTOR_ARM2_2.write(165); // Arm 2
+  delay(200);
+  R_SERVO_MOTOR_WRIST_3.write(180); // Wrist
+  delay(200);
+  R_SERVO_MOTOR_LEFT_CLAW_4.write(115); // Left claw
+  delay(200);
+  R_SERVO_MOTOR_RIGHT_CLAW_5.write(70); // Right claw
+  delay(200);
+
+  Serial.println("pick_object called");
+}
+
+void right_arm_drop_object(){
+  
+  R_SERVO_MOTOR_ARM2_2.write(150); // Arm 2
+  delay(200);
+  R_SERVO_MOTOR_ARM1_1.write(40); // Arm 1
+  delay(200);
+  R_SERVO_MOTOR_BASE_0.write(90); // Base
+  delay(200);
+  R_SERVO_MOTOR_WRIST_3.write(180); // Wrist
+  delay(200);
+  R_SERVO_MOTOR_LEFT_CLAW_4.write(90); // Left claw
+  delay(200);
+  R_SERVO_MOTOR_RIGHT_CLAW_5.write(90); // Right claw
+  delay(200);
 
   Serial.println("drop_object called");
 }
--- Robotic_Arm_Interface_Code/main.py
+++ Robotic_Arm_Interface_Code/main.py
@@ -3,12 +3,20 @@ import gradio as gr
 import serial as sr
 import json
 
-arduino = sr.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
-time.sleep(2)
+try:
+    arduino = sr.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1)
+    time.sleep(2)
+except Exception as e:
+    print("port ttyUSB0 not available")
+    try:
+        arduino = sr.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+        time.sleep(2)
+    except:
+        print("port ttyACM0 not available")
 
-def set_base_angle(angle):
+def set_left_arm_base_angle(angle):
     data = {
-        "motor":"base",
+        "motor":"l_base",
         "angle":angle
     }
     json_str = json.dumps(data)+'\n'
@@ -19,9 +27,9 @@ def set_base_angle(angle):
     print("data received: ", response)
     # return response
 
-def set_arm1_angle(angle):
+def set_left_arm_arm1_angle(angle):
     data = {
-        "motor":"arm1",
+        "motor":"l_arm1",
         "angle":angle
     }
     json_str = json.dumps(data)+'\n'
@@ -32,9 +40,9 @@ def set_arm1_angle(angle):
     print("data received: ", response)
     # return response
 
-def set_arm2_angle(angle):
+def set_left_arm_arm2_angle(angle):
     data = {
-        "motor":"arm2",
+        "motor":"l_arm2",
         "angle":angle
     }
     json_str = json.dumps(data)+'\n'
@@ -45,9 +53,9 @@ def set_arm2_angle(angle):
     print("data received: ", response)
     # return response
 
-def set_wrist_angle(angle):
+def set_left_arm_wrist_angle(angle):
     data = {
-        "motor":"wrist",
+        "motor":"l_wrist",
         "angle":angle
     }
     json_str = json.dumps(data)+'\n'
@@ -58,9 +66,9 @@ def set_wrist_angle(angle):
     print("data received: ", response)
     # return response
 
-def set_left_claw_angle(angle):
+def set_left_arm_left_claw_angle(angle):
     data = {
-        "motor":"left_claw",
+        "motor":"l_left_claw",
         "angle":angle
     }
     json_str = json.dumps(data)+'\n'
@@ -71,9 +79,9 @@ def set_left_claw_angle(angle):
     print("data received: ", response)
     # return response
 
-def set_right_claw_angle(angle):
+def set_left_arm_right_claw_angle(angle):
     data = {
-        "motor":"right_claw",
+        "motor":"l_right_claw",
         "angle":angle
     }
     json_str = json.dumps(data)+'\n'
@@ -84,65 +92,228 @@ def set_right_claw_angle(angle):
     print("data received: ", response)
     # return response
 
-def set_default_position():
+def set_left_arm_default_position():
     data = {
-        "motor":"set_default_position",
+        "motor":"l_set_default_position",
         "angle":"0"
     }
     json_str = json.dumps(data)+'\n'
     arduino.write(json_str.encode())
     print("Set default position")
 
-def pick_object():
+def left_arm_pick_object():
     data = {
-        "motor":"pick_object",
+        "motor":"l_pick_object",
         "angle":"0"
     }
     json_str = json.dumps(data)+'\n'
     arduino.write(json_str.encode())   
     print("Pick Object")
 
-def drop_object():
+def left_arm_drop_object():
     data = {
-        "motor":"drop_object",
+        "motor":"l_drop_object",
+        "angle":"0"
+    }
+    json_str = json.dumps(data)+'\n'
+    arduino.write(json_str.encode())
+    print("Drop Object")
+    
+def left_arm_hold_leaf():
+    ...
+
+def left_arm_fold_leaf():
+    ...
+
+def left_arm_leave_leaf():
+    ...
+
+# ------------------------------------------Right Arm Code---------------------------------
+
+def set_right_arm_base_angle(angle):
+    data = {
+        "motor":"r_base",
+        "angle":angle
+    }
+    json_str = json.dumps(data)+'\n'
+    arduino.write(json_str.encode()) 
+    time.sleep(0.2)
+    response = arduino.readline().decode().strip()
+    print("data sent: ", json_str)
+    print("data received: ", response)
+    # return response
+
+def set_right_arm_arm1_angle(angle):
+    data = {
+        "motor":"r_arm1",
+        "angle":angle
+    }
+    json_str = json.dumps(data)+'\n'
+    arduino.write(json_str.encode()) 
+    time.sleep(0.2)
+    response = arduino.readline().decode().strip()
+    print("data sent: ", json_str)
+    print("data received: ", response)
+    # return response
+
+def set_right_arm_arm2_angle(angle):
+    data = {
+        "motor":"r_arm2",
+        "angle":angle
+    }
+    json_str = json.dumps(data)+'\n'
+    arduino.write(json_str.encode()) 
+    time.sleep(0.2)
+    response = arduino.readline().decode().strip()
+    print("data sent: ", json_str)
+    print("data received: ", response)
+    # return response
+
+def set_right_arm_wrist_angle(angle):
+    data = {
+        "motor":"r_wrist",
+        "angle":angle
+    }
+    json_str = json.dumps(data)+'\n'
+    arduino.write(json_str.encode()) 
+    time.sleep(0.2)
+    response = arduino.readline().decode().strip()
+    print("data sent: ", json_str)
+    print("data received: ", response)
+    # return response
+
+def set_right_arm_left_claw_angle(angle):
+    data = {
+        "motor":"r_left_claw",
+        "angle":angle
+    }
+    json_str = json.dumps(data)+'\n'
+    arduino.write(json_str.encode()) 
+    time.sleep(0.2)
+    response = arduino.readline().decode().strip()
+    print("data sent: ", json_str)
+    print("data received: ", response)
+    # return response
+
+def set_right_arm_right_claw_angle(angle):
+    data = {
+        "motor":"r_right_claw",
+        "angle":angle
+    }
+    json_str = json.dumps(data)+'\n'
+    arduino.write(json_str.encode()) 
+    time.sleep(0.2)
+    response = arduino.readline().decode().strip()
+    print("data sent: ", json_str)
+    print("data received: ", response)
+    # return response
+
+def set_right_arm_default_position():
+    data = {
+        "motor":"r_set_default_position",
+        "angle":"0"
+    }
+    json_str = json.dumps(data)+'\n'
+    arduino.write(json_str.encode())
+    print("Set default position")
+
+def right_arm_pick_object():
+    data = {
+        "motor":"r_pick_object",
+        "angle":"0"
+    }
+    json_str = json.dumps(data)+'\n'
+    arduino.write(json_str.encode())   
+    print("Pick Object")
+
+def right_arm_drop_object():
+    data = {
+        "motor":"r_drop_object",
         "angle":"0"
     }
     json_str = json.dumps(data)+'\n'
     arduino.write(json_str.encode())
     print("Drop Object")
 
+def right_arm_hold_leaf():
+    ...
+
+def right_arm_fold_leaf():
+    ...
+
+def right_arm_leave_leaf():
+    ...
+
+
 with gr.Blocks() as app:
     with gr.Row():
-        input_base = gr.Text(label="Base")
-        button_base = gr.Button("Set Base")
-    with gr.Row():
-        input_arm1 = gr.Text(label="Arm1")
-        button_arm1 = gr.Button("Set Arm1")
-    with gr.Row():
-        input_arm2 = gr.Text(label="Arm2")
-        button_arm2 = gr.Button("Set Arm2")
-    with gr.Row():        
-        input_wrist = gr.Text(label="Wrist")
-        button_wrist = gr.Button("Set Wrist")
-    with gr.Row():
-        input_claw_left = gr.Text(label="Left Claw")
-        button_claw_left = gr.Button("Set Left Claw")
-    with gr.Row():
-        input_claw_right = gr.Text(label="Right Claw")
-        button_claw_right = gr.Button("Set Right Claw")
-    with gr.Row():
-        button_set_default_position = gr.Button("Set Default Position")
-        button_pick_object = gr.Button("Pick Object")
-        button_drop_object = gr.Button("Drop Object")
+        with gr.Column():
+            gr.Markdown("# **Left Arm**")
+        
+            left_arm_input_base = gr.Text(label="Base")
+            left_arm_button_base = gr.Button("Set Base")
+        
+            left_arm_input_arm1 = gr.Text(label="Arm1")
+            left_arm_button_arm1 = gr.Button("Set Arm1")
+        
+            left_arm_input_arm2 = gr.Text(label="Arm2")
+            left_arm_button_arm2 = gr.Button("Set Arm2")
+        
+            left_arm_input_wrist = gr.Text(label="Wrist")
+            left_arm_button_wrist = gr.Button("Set Wrist")
+        
+            left_arm_input_claw_left = gr.Text(label="Left Claw")
+            left_arm_button_claw_left = gr.Button("Set Left Claw")
+        
+            left_arm_input_claw_right = gr.Text(label="Right Claw")
+            left_arm_button_claw_right = gr.Button("Set Right Claw")
+        
+            left_arm_button_set_default_position = gr.Button("Set Default Position")
+            left_arm_button_pick_object = gr.Button("Pick Object")
+            left_arm_button_drop_object = gr.Button("Drop Object")
 
-    button_base.click(fn=set_base_angle, inputs=input_base)
-    button_arm1.click(fn=set_arm1_angle, inputs=input_arm1)
-    button_arm2.click(fn=set_arm2_angle, inputs=input_arm2)
-    button_wrist.click(fn=set_wrist_angle, inputs=input_wrist)
-    button_claw_left.click(fn=set_left_claw_angle, inputs=input_claw_left)
-    button_claw_right.click(fn=set_right_claw_angle, inputs=input_claw_right)
-    button_set_default_position.click(fn=set_default_position)
-    button_pick_object.click(fn=pick_object)
-    button_drop_object.click(fn=drop_object)
+            left_arm_button_base.click(fn=set_left_arm_base_angle, inputs=left_arm_input_base)
+            left_arm_button_arm1.click(fn=set_left_arm_arm1_angle, inputs=left_arm_input_arm1)
+            left_arm_button_arm2.click(fn=set_left_arm_arm2_angle, inputs=left_arm_input_arm2)
+            left_arm_button_wrist.click(fn=set_left_arm_wrist_angle, inputs=left_arm_input_wrist)
+            left_arm_button_claw_left.click(fn=set_left_arm_left_claw_angle, inputs=left_arm_input_claw_left)
+            left_arm_button_claw_right.click(fn=set_left_arm_right_claw_angle, inputs=left_arm_input_claw_right)
+            left_arm_button_set_default_position.click(fn=set_left_arm_default_position)
+            left_arm_button_pick_object.click(fn=left_arm_pick_object)
+            left_arm_button_drop_object.click(fn=left_arm_drop_object)
+            
+        with gr.Column():
+            gr.Markdown("# **Right Arm**")
+        
+            right_arm_input_base = gr.Text(label="Base")
+            right_arm_button_base = gr.Button("Set Base")
+        
+            right_arm_input_arm1 = gr.Text(label="Arm1")
+            right_arm_button_arm1 = gr.Button("Set Arm1")
+        
+            right_arm_input_arm2 = gr.Text(label="Arm2")
+            right_arm_button_arm2 = gr.Button("Set Arm2")
+        
+            right_arm_input_wrist = gr.Text(label="Wrist")
+            right_arm_button_wrist = gr.Button("Set Wrist")
+        
+            right_arm_input_claw_left = gr.Text(label="Left Claw")
+            right_arm_button_claw_left = gr.Button("Set Left Claw")
+        
+            right_arm_input_claw_right = gr.Text(label="Right Claw")
+            right_arm_button_claw_right = gr.Button("Set Right Claw")
+        
+            right_arm_button_set_default_position = gr.Button("Set Default Position")
+            right_arm_button_pick_object = gr.Button("Pick Object")
+            right_arm_button_drop_object = gr.Button("Drop Object")
 
+            right_arm_button_base.click(fn=set_right_arm_base_angle, inputs=right_arm_input_base)
+            right_arm_button_arm1.click(fn=set_right_arm_arm1_angle, inputs=right_arm_input_arm1)
+            right_arm_button_arm2.click(fn=set_right_arm_arm2_angle, inputs=right_arm_input_arm2)
+            right_arm_button_wrist.click(fn=set_right_arm_wrist_angle, inputs=right_arm_input_wrist)
+            right_arm_button_claw_left.click(fn=set_right_arm_left_claw_angle, inputs=right_arm_input_claw_left)
+            right_arm_button_claw_right.click(fn=set_right_arm_right_claw_angle, inputs=right_arm_input_claw_right)
+            right_arm_button_set_default_position.click(fn=set_right_arm_default_position)
+            right_arm_button_pick_object.click(fn=right_arm_pick_object)
+            right_arm_button_drop_object.click(fn=right_arm_drop_object)
 app.launch()
